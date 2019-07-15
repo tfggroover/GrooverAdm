e3f0fe467c7f18293f6b9cea5fdb9d55493d8d74
Amartin <amartingordillo@gmail.com>
2019-07-15 13:25:40 +0200

    Add project files.

---
+++ GrooverAdm/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GrooverAdm.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
