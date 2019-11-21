80b2e4ec4a71c4a737848feeb233d055a5641905
Dalmaxor <amartingordillo@gmail.com>
2019-11-21 15:11:50 +0100

    Fix expected type of images

--- GrooverAdmSPA/Model/SpotifyUserInfo.cs
+++ GrooverAdmSPA/Model/SpotifyUserInfo.cs
@@ -14,7 +14,7 @@ namespace GrooverAdmSPA.Model
         public Dictionary<string,string> External_urls { get; set; }
         public string Href { get; set; }
         public string Id { get; set; }
-        public List<string> Images { get; set; }
+        public List<Image> Images { get; set; }
         public string Product { get; set; } //Probablemente sea mejor un enum
         public string Type { get; set; }
         public string Uri { get; set; } //Solo útil en un móvil
@@ -25,5 +25,12 @@ namespace GrooverAdmSPA.Model
     {
         public bool Filter_enabled { get; set; }
         public bool Filter_locked { get; set; }
+    }
+
+    public class Image
+    {
+        public int? Height { get; set; }
+        public int? Width { get; set; }
+        public string Url { get; set; }
     }
 }
