ad8807604c4ea3aec997c48d1924c74ff4ed0621
Amartin <amartingordillo@gmail.com>
2019-11-24 18:41:39 +0100

    Added services to calculate the distances for the "near establishments" endpoints

---
+++ GrooverAdmSPA/Services/DistanceService.cs
@@ -0,0 +1,39 @@
+﻿using Google.Cloud.Firestore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GrooverAdmSPA.Services
+{
+    public class DistanceService
+    {
+
+        public static double Distance(GeoPoint point1, GeoPoint point2)
+        {
+            GeopointService.ValidateGeopoint(point1);
+            GeopointService.ValidateGeopoint(point2);
+
+            var R = 6371e3; // metres
+            var φ1 = DegreesToRadians(point1.Latitude);
+            var φ2 = DegreesToRadians(point2.Latitude);
+            var Δφ = DegreesToRadians(point2.Latitude - point1.Longitude);
+            var Δλ = DegreesToRadians(point2.Longitude - point1.Longitude);
+
+            var a = Math.Sin(Δφ / 2) * Math.Sin(Δφ / 2) +
+                    Math.Cos(φ1) * Math.Cos(φ2) *
+                    Math.Sin(Δλ / 2) * Math.Sin(Δλ / 2);
+            var c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
+
+            var d = R * c;
+
+            return d;
+        }
+
+
+        private static double DegreesToRadians(double deg)
+        {
+            return deg * (Math.PI / 180);
+        }
+    }
+}
