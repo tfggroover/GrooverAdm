06337a4799103198ebfe411557076508fcc86758
Amartin <amartingordillo@gmail.com>
2020-03-15 20:27:43 +0100

    Update to Angular 9

--- GrooverAdmSPA/ClientApp/src/app/app.module.ts
+++ GrooverAdmSPA/ClientApp/src/app/app.module.ts
@@ -9,6 +9,8 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { PlacesModule } from './places/places.module';
+import { PlacesComponent } from './places/places/places.component';
 
 @NgModule({
   declarations: [
@@ -24,9 +26,10 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
     FormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
+      { path: 'places', component: PlacesComponent },
       { path: 'fetch-data', component: FetchDataComponent },
-    ])
+    ]),
+    PlacesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
