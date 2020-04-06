75eab53bc5d5faafd8583ac5cc47df1198ce39f0
Dalmaxor <amartingordillo@gmail.com>
2020-04-06 09:52:46 +0200

    GenreController

---
+++ GrooverAdmSPA/Controllers/GenreController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GrooverAdmSPA.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GrooverAdmSPA.Controllers
+{
+    [Route("api/[controller]")]
+    public class GenreController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index(List<Song> songs)
+        {
+
+            throw new NotImplementedException("Jokes on you");
+            return Ok();
+        }
+    }
+}
