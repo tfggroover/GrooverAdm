484c770e6d9dc2c646da039a08b4d6001699424f
aMartin <amartingordillo@gmail.com>
2020-08-18 18:35:46 +0200

    Añadir controlador base para poder obtener el userId del token

---
+++ Fake/Controllers/ControllerBase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fake.Controllers
+{
+    public class ControllerBase : Microsoft.AspNetCore.Mvc.ControllerBase
+    {
+        /// <summary>
+        /// Get authenticated user id.        
+        /// </summary>
+        /// <returns></returns>
+        protected string GetUserId()
+        {
+            var userId = User.Claims.FirstOrDefault(x => x.Type == "user_id")?.Value;
+            if (string.IsNullOrEmpty(userId))
+            {
+                throw new UnauthorizedAccessException();
+            }
+
+            return userId;
+        }
+    }
+}
