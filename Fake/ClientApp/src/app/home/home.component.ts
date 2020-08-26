3352e0c3179fe65fc5cdfa7ac13469c3512828c7
aMartin <amartingordillo@gmail.com>
2020-08-26 02:13:07 +0200

    Test auth button

--- Fake/ClientApp/src/app/home/home.component.ts
+++ Fake/ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { HttpClient, HttpBackend } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
+
+  public HomeComponent(handler: HttpBackend, @Inject('BASE_URL') baseUrl: string) {
+    const http = new HttpClient(handler);
+    http.get(baseUrl + 'home/auth').subscribe(result => {
+      console.log(result);
+    }, error => console.error(error));
+  }
+
+  public onSignInButtonClick() {
+    // Open the Auth flow in a popup.
+    window.open('/home/auth', 'firebaseAuth', 'height=315,width=400');
+  }
 }
