fb1f3738ed2e7193d102e5886663cfba4007340f
aMartin <amartingordillo@gmail.com>
2020-08-30 13:22:20 +0200

    Add new exceptions for auth

---
+++ GrooverAdm.Common/GrooverAuthException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GrooverAdm.Common
+{
+    public class GrooverAuthException: GrooverException
+    {
+        public GrooverAuthException() : base(401) { }
+        public GrooverAuthException(string message) : base(message, 401) { }
+    }
+}
--- GrooverAdm.Common/GrooverException.cs
+++ GrooverAdm.Common/GrooverException.cs
@@ -4,7 +4,16 @@ namespace GrooverAdm.Common
 {
     public class GrooverException : Exception
     {
-        public GrooverException() : base() { }
-        public GrooverException(string message) : base(message) { }
+        public int HttpCode { get; set; }
+        public GrooverException() : base() { this.HttpCode = 400; }
+        public GrooverException(string message) : base(message) { this.HttpCode = 400; }
+        public GrooverException(int httpCode) : base() 
+        {
+            this.HttpCode = httpCode;
+        }
+        public GrooverException(string message, int httpCode) : base(message) 
+        {
+            this.HttpCode = httpCode;
+        }
     }
 }
