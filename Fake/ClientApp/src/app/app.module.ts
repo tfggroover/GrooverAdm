cfaf1ad6182078c519008c51b4b8a0797ad13d7a
aMartin <amartingordillo@gmail.com>
2020-08-31 00:18:31 +0200

    Fix module

--- Fake/ClientApp/src/app/app.module.ts
+++ Fake/ClientApp/src/app/app.module.ts
@@ -7,8 +7,6 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { ApiAuthorizationModule } from 'src/api-authorization/api-authorization.module';
 import { AuthorizeGuard } from 'src/api-authorization/authorize.guard';
 import { AngularFireModule } from '@angular/fire';
@@ -45,8 +43,6 @@ export const firebaseConfig = {
     AngularFireAuthModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent, canActivate: [AuthorizeGuard] },
       {
         path: 'place',
         loadChildren: () => import('./places/places.module').then(m => m.PlacesModule)
