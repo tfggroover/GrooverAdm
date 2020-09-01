57181ea6c7c67aa23455b44751931d8ef80aeca1
aMartin <amartingordillo@gmail.com>
2020-09-01 22:15:53 +0200

    Fix guard

--- Fake/ClientApp/src/app/places/services/owner.guard.ts
+++ Fake/ClientApp/src/app/places/services/owner.guard.ts
@@ -2,9 +2,10 @@ import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable, combineLatest } from 'rxjs';
 import { PlaceService } from './place.service';
-import { AuthorizeService, User } from 'src/api-authorization/authorize.service';
+import { AuthorizeService } from 'src/api-authorization/authorize.service';
 import { Place } from 'src/app/services/services';
 import { map } from 'rxjs/operators';
+import { CompleteUser } from 'src/api-authorization/user-manager/userManagerService';
 
 @Injectable({
   providedIn: 'root'
@@ -19,7 +20,7 @@ export class OwnerGuard implements CanActivate {
     return combineLatest([this.placeService.getPlace(next.params.id), this.auth.getUser()]).pipe(map((p) => this.isUserOwner(p[0], p[1])));
   }
 
-  isUserOwner(place: Place, user: User): boolean {
+  isUserOwner(place: Place, user: CompleteUser): boolean {
     return place.owners.map(u => u.id).indexOf(user.firebaseAuthentication.uid) !== -1;
   }
 
