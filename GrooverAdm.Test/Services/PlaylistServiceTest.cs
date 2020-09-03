c594ac647350ef62cf6cc5bf41840e8f45c4f52a
aMartin <amartingordillo@gmail.com>
2020-09-03 13:10:58 +0200

    Unit testing

--- GrooverAdm.Test/Services/PlaylistServiceTest.cs
+++ GrooverAdm.Test/Services/PlaylistServiceTest.cs
@@ -1,7 +1,16 @@
-﻿using NUnit.Framework;
+﻿using GrooverAdm.Business.Services.Playlist;
+using GrooverAdm.Business.Services.Song;
+using GrooverAdm.Business.Services.User;
+using GrooverAdm.DataAccess.Dao;
+using GrooverAdm.DataAccess.Firestore.Dao;
+using GrooverAdm.DataAccess.Firestore.Model;
+using GrooverAdm.Mappers.Interface;
+using Moq;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace GrooverAdm.Test.Services
 {
@@ -9,5 +18,62 @@ namespace GrooverAdm.Test.Services
 
     public class PlaylistServiceTest
     {
+        private Mock<IPlaylistDao<Playlist>> dao;
+        private Mock<IPlaylistMapper<Playlist>> mapper;
+        private Mock<ISongService> song;
+        private IPlaylistService playlistService;
+
+        [SetUp]
+        public void SetUp()
+        {
+            string place = "test";
+            var dbplaylist = new Playlist();
+            var playlist = new Entities.Application.Playlist();
+            dao = new Mock<IPlaylistDao<Playlist>>();
+            mapper = new Mock<IPlaylistMapper<Playlist>>();
+            song = new Mock<ISongService>();
+            playlistService = new PlaylistService(dao.Object, mapper.Object, song.Object, null);
+
+            dao.Setup(a => a.GetPlaylist(place)).Returns(Task.FromResult(dbplaylist));
+            dao.Setup(a => a.GetPlaylist("")).Returns(Task.FromResult<Playlist>(null));
+            mapper.Setup(a => a.ToApplicationEntity(dbplaylist)).Returns(playlist);
+            song.Setup(a => a.GetSongsFromPlaylist(place, "mainPlaylist", 1, int.MaxValue)).Returns(() => Task.FromResult(new List<Entities.Application.Song> { new Entities.Application.Song() }));
+        }
+
+        [Test]
+        public async Task GetMainPlaylistFromPlace_ExistingPlace_NoSongs()
+        {
+
+            var res = await playlistService.GetMainPlaylistFromPlace("test", false, 1, int.MaxValue);
+
+            Assert.IsNotNull(res);
+            Assert.IsNotNull(res.Songs);
+            Assert.IsFalse(res.Changed);
+
+        }
+
+        [Test]
+        public async Task GetMainPlaylistFromPlace_ExistingPlace_Songs()
+        {
+            var res = await playlistService.GetMainPlaylistFromPlace("test", true, 1, int.MaxValue);
+
+            Assert.IsNotNull(res);
+            Assert.IsNotNull(res.Songs);
+            Assert.IsFalse(res.Changed);
+            Assert.IsNotEmpty(res.Songs);
+        }
+
+        [Test]
+        public async Task GetMainPlaylistFromPlace_NonExistingPlace_Null()
+        {
+
+
+            var res = await playlistService.GetMainPlaylistFromPlace("", true, 1, int.MaxValue);
+
+            Assert.IsNull(res);
+        }
+
+
+
     }
 }
--- GrooverAdm.Test/Services/UserServiceTest.cs
+++ GrooverAdm.Test/Services/UserServiceTest.cs
@@ -1,16 +1,12 @@
 ﻿using FirebaseAdmin;
-using Google.Cloud.Firestore.V1;
 using GrooverAdm.Business.Services.User;
+using GrooverAdm.Common;
 using GrooverAdm.DataAccess.Dao;
 using GrooverAdm.DataAccess.Firestore.Model;
 using GrooverAdm.Mappers.Interface;
 using Moq;
 using NUnit.Framework;
-using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
-using System.Xml.XPath;
 
 namespace GrooverAdm.Test.Services
 {
@@ -19,7 +15,6 @@ namespace GrooverAdm.Test.Services
     {
         private Mock<IUserMapper<DataAccess.Firestore.Model.User>> _userMapper;
         private Mock<IUserDao<DataAccess.Firestore.Model.User>> _userDao;
-        private Mock<FirebaseApp> _firebase;
 
         private IUserService userService;
 
@@ -28,35 +23,48 @@ namespace GrooverAdm.Test.Services
         {
             _userMapper = new Mock<IUserMapper<DataAccess.Firestore.Model.User>>();
             _userDao = new Mock<IUserDao<DataAccess.Firestore.Model.User>>();
-            _firebase = new Mock<FirebaseApp>();
         }
 
         [Test]
-        public void NameAdmin_ValidUser_Ok()
+        public async Task NameAdmin_ValidUser_Ok()
         {
             //Act
             var id = "1";
             User currDbUser = new User { Admin = true};
-            _userDao.Setup(a => a.GetUser(id)).Returns(Task.FromResult(currDbUser));
-            _userDao.Setup(a => a.SetAdmin(id)).Returns(Task.FromResult(currDbUser));
             User dbUser = new User { Admin = true };
             GrooverAdm.Entities.Application.User result = new Entities.Application.User { Admin = true };
+            _userDao.Setup(a => a.GetUser(id)).Returns(Task.FromResult(currDbUser));
+            _userDao.Setup(a => a.SetAdmin(id)).Returns(Task.FromResult(currDbUser));
             _userMapper.Setup(a => a.ToApplicationEntity(dbUser)).Returns(result);
 
             //Arrange
 
-            IUserService userService = new UserService(_userDao.Object, _userMapper.Object, _firebase.Object);
-            var res = userService.NameAdmin(id, id);
+            IUserService userService = new UserService(_userDao.Object, _userMapper.Object, null); //This makes so that we can't test fireAuth methods, sucks
+            var res = await userService.NameAdmin(id, id);
 
             //Assert
-
+            Assert.IsTrue(res.Admin);
 
         }
 
         [Test]
         public void NameAdmin_InvalidUser_NotOk()
         {
+            //Act
+            var id = "1";
+            User currDbUser = new User { Admin = false };
+            User dbUser = new User { Admin = true };
+            GrooverAdm.Entities.Application.User result = new Entities.Application.User { Admin = true };
+            _userDao.Setup(a => a.GetUser(id)).Returns(Task.FromResult(currDbUser));
+            _userDao.Setup(a => a.SetAdmin(id)).Returns(Task.FromResult(currDbUser));
+            _userMapper.Setup(a => a.ToApplicationEntity(dbUser)).Returns(result);
 
+            //Arrange
+
+            IUserService userService = new UserService(_userDao.Object, _userMapper.Object, null); //This makes so that we can't test fireAuth methods, sucks
+
+            //Assert
+            Assert.ThrowsAsync<GrooverAuthException>(async () => await userService.NameAdmin(id, id));
         }
 
 
