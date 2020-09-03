34489f688bee7ea15ea189ec846c77cc89bd375c
aMartin <amartingordillo@gmail.com>
2020-09-03 14:12:00 +0200

    tests

--- GrooverAdm.Test/Services/DistanceServiceTest.cs
+++ GrooverAdm.Test/Services/DistanceServiceTest.cs
@@ -1,4 +1,5 @@
-﻿using GrooverAdmSPA.Business.Services;
+﻿using GrooverAdm.Entities.Application;
+using GrooverAdmSPA.Business.Services;
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
@@ -20,9 +21,16 @@ namespace GrooverAdm.Test.Services
         [TestCaseSource("invalid")]
         public void ValidateGeohash_InvalidInput_ThowsException(string geo)
         {
-            Assert.Throws(typeof(ArgumentException), delegate {
+            Assert.Throws(typeof(ArgumentException), delegate
+            {
                 DistanceService.ValidateGeohash(geo);
             });
+        }
+
+        [TestCaseSource("queries")]
+        public void GeohashQueries_ValidInput_NoException(Geolocation geolocation, double radius)
+        {
+            DistanceService.GeohashQueries(geolocation, radius);
         }
 
 
@@ -37,5 +45,56 @@ namespace GrooverAdm.Test.Services
             "as5dads+1-",
             "asd?!ads"
             };
+
+        private static IEnumerable<object[]> queries()
+        {
+            yield return
+                new object[]
+                {
+                    new Geolocation(0,0), 500
+                };
+            yield return
+                new object[]
+                {
+                    new Geolocation(0,0), 5000
+                };
+            yield return
+                new object[]
+                {
+                    new Geolocation(0,0), 50000
+                };
+            yield return
+                new object[]
+                {
+                    new Geolocation(50,15), 500
+                };
+            yield return
+                new object[]
+                {
+                    new Geolocation(50,15), 5000
+                };
+            yield return
+                new object[]
+                {
+                    new Geolocation(50,15), 50000
+                };
+            yield return
+                new object[]
+                {
+                    new Geolocation(15,50), 5000
+                };
+            yield return
+                new object[]
+                {
+                    new Geolocation(80,5), 500
+                };
+            yield return
+                new object[]
+                {
+                    new Geolocation(80, 15), 5000000
+                };
+
+
+        }
     }
 }
--- GrooverAdm.Test/Services/PlaceServiceTest.cs
+++ GrooverAdm.Test/Services/PlaceServiceTest.cs
@@ -1,4 +1,14 @@
-﻿using NUnit.Framework;
+﻿using GrooverAdm.Business.Services;
+using GrooverAdm.Business.Services.Places;
+using GrooverAdm.Business.Services.Playlist;
+using GrooverAdm.Business.Services.Rating;
+using GrooverAdm.Business.Services.Song;
+using GrooverAdm.Business.Services.User;
+using GrooverAdm.DataAccess.Dao;
+using GrooverAdm.DataAccess.Firestore.Model;
+using GrooverAdm.Mappers.Interface;
+using Moq;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -9,5 +19,35 @@ namespace GrooverAdm.Test.Services
 
     public class PlaceServiceTest
     {
+
+        private Mock<IPlacesDao<DataAccess.Firestore.Model.Place>> _dao;
+        private Mock<IPlaceMapper<DataAccess.Firestore.Model.Place>> _mapper;
+        private Mock<IRatingService> ratingService;
+        private Mock<IUserService> userService;
+        private Mock<IPlaylistService> playlistService;
+        private Mock<ISongService> songService;
+        private Mock<RecommendationService> recommendationService;
+
+        private IPlacesService placeService;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _dao = new Mock<IPlacesDao<DataAccess.Firestore.Model.Place>>();
+            _mapper = new Mock<IPlaceMapper<DataAccess.Firestore.Model.Place>>();
+            ratingService = new Mock<IRatingService>();
+            userService = new Mock<IUserService>();
+            playlistService = new Mock<IPlaylistService>();
+            songService = new Mock<ISongService>();
+            recommendationService = new Mock<RecommendationService>();
+
+            placeService = new PlacesService(_dao.Object, _mapper.Object, playlistService.Object, songService.Object, recommendationService.Object, userService.Object, ratingService.Object, null);    
+        }
+
+
+        
+
+        
+
     }
 }
