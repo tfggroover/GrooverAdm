766b605409599b8030de39e392b8405426d7066e
aMartin <amartingordillo@gmail.com>
2020-09-03 11:19:58 +0200

    Change menu

--- Fake/ClientApp/src/app/nav-menu/nav-menu.component.ts
+++ Fake/ClientApp/src/app/nav-menu/nav-menu.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthorizeService } from 'src/api-authorization/authorize.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-nav-menu',
   templateUrl: './nav-menu.component.html',
   styleUrls: ['./nav-menu.component.css']
 })
-export class NavMenuComponent {
+export class NavMenuComponent implements OnInit {
   isExpanded = false;
+  public isAuthenticated: Observable<boolean>;
+  constructor(private auth: AuthorizeService) {}
+
+  ngOnInit(): void {
+    this.isAuthenticated = this.auth.isAuthenticated();
+
+  }
 
   collapse() {
     this.isExpanded = false;
